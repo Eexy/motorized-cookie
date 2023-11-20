94e2c94e5b930b4a78537b3eed4925b45a686d92
Isai Boussif <eexdhe@gmail.com>
2023-11-20 20:41:39 +0100

    feat: create zod validation pipe and use it in auth controller for signup route

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,13 +1,20 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { CreateClientDto } from '../clients/dto/create-client.dto';
+import {
+  CreateClientDto,
+  createClientDtoSchema,
+} from '../clients/dto/create-client.dto';
 import { AuthService } from './auth.service';
+import { ZodValidationPipe } from '../common/pipes/zod-validation.pipe';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('signup')
-  async signup(@Body() createClientDto: CreateClientDto) {
+  async signup(
+    @Body(new ZodValidationPipe(createClientDtoSchema))
+    createClientDto: CreateClientDto,
+  ) {
     console.log(createClientDto);
     return await this.authService.signup({
       email: 'test',
