72c74812a1e5e42dbd5ce6f96d6cbc416c412c6d
Isai Boussif <eexdhe@gmail.com>
2023-11-21 21:17:50 +0100

    feat: use icon instead of text

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,7 +3,10 @@ import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
 import {provideHttpClient} from "@angular/common/http";
+import {provideNgIconsConfig} from "@ng-icons/core";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient()]
+  providers: [provideRouter(routes), provideHttpClient(), provideNgIconsConfig({
+    size: "1.5em"
+  })]
 };
--- frontend/src/app/components/app-header/components/header-ctas/header-ctas.component.ts
+++ frontend/src/app/components/app-header/components/header-ctas/header-ctas.component.ts
@@ -1,11 +1,15 @@
 import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterLink} from "@angular/router";
+import {NgIconComponent, provideIcons} from "@ng-icons/core";
+import {heroUser} from "@ng-icons/heroicons/outline";
 
 @Component({
   selector: 'header-ctas',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, NgIconComponent
+  ],
+  providers: [provideIcons({heroUser})],
   templateUrl: './header-ctas.component.html',
 })
 export class HeaderCtasComponent {
