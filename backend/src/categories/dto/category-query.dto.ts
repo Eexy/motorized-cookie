aa68ae75dc9507a44101eea59d0a610c0cda67f9
Isai Boussif <eexdhe@gmail.com>
2023-11-21 22:45:38 +0100

    fix: set default value on category schema

--- backend/src/categories/dto/category-query.dto.ts
+++ backend/src/categories/dto/category-query.dto.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 export const categoryQueryDtoSchema = z.object({
   limit: z.number().int().min(1).default(25),
-  page: z.number().int().min(1),
+  page: z.number().int().min(1).default(1),
 });
 
 export type CategoryQueryDto = z.infer<typeof categoryQueryDtoSchema>;
