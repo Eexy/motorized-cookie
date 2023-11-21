211d929e4b903d1172afa039a40a60e8fd0d5a26
Isai Boussif <eexdhe@gmail.com>
2023-11-21 18:21:04 +0100

    feat: redirect user to homepage on signin/signup

--- frontend/src/app/auth/services/auth-service.service.ts
+++ frontend/src/app/auth/services/auth-service.service.ts
@@ -3,6 +3,7 @@ import {HttpClient} from "@angular/common/http";
 import {SignupDto} from "../dto/signup.dto";
 import {SigninDto} from "../dto/signin.dto";
 import {BehaviorSubject} from "rxjs";
+import {Router} from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
@@ -10,19 +11,20 @@ import {BehaviorSubject} from "rxjs";
 export class AuthService {
   private token = new BehaviorSubject<string | null>(null)
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private router: Router) {
   }
 
   async signup(signupDto: SignupDto) {
-    console.log(signupDto)
     this.http.post<{ token: string }>('http://127.0.0.1:3000/auth/signup', signupDto).subscribe(res => {
       this.token.next(res.token)
+      this.router.navigate([""])
     })
   }
 
   async signin(signinDto: SigninDto) {
     this.http.post<{ token: string }>('http://127.0.0.1:3000/auth/signup', signinDto).subscribe(res => {
       this.token.next(res.token)
+      this.router.navigate([""])
     })
   }
 
