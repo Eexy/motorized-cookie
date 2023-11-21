a214953d39915a057a1593772f9e39a25c34ef7b
Isai Boussif <eexdhe@gmail.com>
2023-11-21 18:25:45 +0100

    refactor: save auth token in localstorage

--- frontend/src/app/auth/services/auth.service.ts
+++ frontend/src/app/auth/services/auth.service.ts
@@ -2,37 +2,34 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {SignupDto} from "../dto/signup.dto";
 import {SigninDto} from "../dto/signin.dto";
-import {BehaviorSubject} from "rxjs";
 import {Router} from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private token = new BehaviorSubject<string | null>(null)
-
   constructor(private http: HttpClient, private router: Router) {
   }
 
   async signup(signupDto: SignupDto) {
     this.http.post<{ token: string }>('http://127.0.0.1:3000/auth/signup', signupDto).subscribe(res => {
-      this.token.next(res.token)
       this.router.navigate([""])
     })
   }
 
   async signin(signinDto: SigninDto) {
     this.http.post<{ token: string }>('http://127.0.0.1:3000/auth/signup', signinDto).subscribe(res => {
-      this.token.next(res.token)
+      localStorage.setItem("token", res.token)
       this.router.navigate([""])
     })
   }
 
   logout() {
-    this.token.next(null)
+    localStorage.removeItem("token")
+    this.router.navigate([""])
   }
 
   getToken() {
-    return this.token.value
+    return localStorage.getItem("token")
   }
 }
