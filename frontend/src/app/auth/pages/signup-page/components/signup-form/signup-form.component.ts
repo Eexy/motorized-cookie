6cea78f654a2d1d09ce1610fd72cbcdbae6821c4
Isai Boussif <eexdhe@gmail.com>
2023-11-21 16:11:35 +0100

    feat: create auth signup form

---
+++ frontend/src/app/auth/pages/signup-page/components/signup-form/signup-form.component.ts
@@ -0,0 +1,27 @@
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'signup-form',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './signup-form.component.html',
+})
+export class SignupFormComponent {
+  signupForm = this.fb.group({
+    firstname: ["", [Validators.required, Validators.minLength(3)]],
+    email: ["", [Validators.required, Validators.email]],
+    password: ["", [Validators.required, Validators.minLength(5)]],
+    lastname: ["", Validators.required],
+    gender: ["", Validators.required]
+  })
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  onSubmit() {
+    console.log(this.signupForm.getRawValue())
+  }
+
+}
--- frontend/src/app/auth/pages/signup-page/signup-page.component.ts
+++ frontend/src/app/auth/pages/signup-page/signup-page.component.ts
@@ -1,10 +1,11 @@
 import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {SignupFormComponent} from "./components/signup-form/signup-form.component";
 
 @Component({
   selector: 'signup-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SignupFormComponent],
   templateUrl: './signup-page.component.html',
 })
 export class SignupPageComponent {
