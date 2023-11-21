2781fd5376b9ced27fc3eaac2ebf3d8ba5538246
Isai Boussif <eexdhe@gmail.com>
2023-11-21 22:40:15 +0100

    feat: load navigation category from database on app init

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,12 +1,34 @@
-import {ApplicationConfig} from '@angular/core';
+import {APP_INITIALIZER, ApplicationConfig} from '@angular/core';
 import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
-import {provideHttpClient} from "@angular/common/http";
+import {HttpClient, provideHttpClient} from "@angular/common/http";
 import {provideNgIconsConfig} from "@ng-icons/core";
+import {NavService} from "./components/app-header/components/app-nav/nav.service";
+import {environment} from "../environments/environment";
+import {Category} from "./components/app-header/components/app-nav/category";
+
+export function initializeApp(navService: NavService, http: HttpClient) {
+  return async () => {
+    console.log("Init")
+    if (!environment.production) {
+      return navService.setCategories([{id: 1, name: "Short"}])
+    }
+
+    return http.get<Category[]>("http://127.0.0.1/categories").subscribe(res => {
+      console.info("here")
+      navService.setCategories(res)
+    })
+  }
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideHttpClient(), provideNgIconsConfig({
     size: "1.5em"
-  })]
+  }), {
+    provide: APP_INITIALIZER,
+    useFactory: initializeApp,
+    multi: true,
+    deps: [NavService, HttpClient]
+  }]
 };
--- frontend/src/app/components/app-header/app-header.component.ts
+++ frontend/src/app/components/app-header/app-header.component.ts
@@ -2,11 +2,12 @@ import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HeaderCtasComponent} from "./components/header-ctas/header-ctas.component";
 import {RouterLink} from "@angular/router";
+import {AppNavComponent} from "./components/app-nav/app-nav.component";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, HeaderCtasComponent, RouterLink],
+  imports: [CommonModule, HeaderCtasComponent, RouterLink, AppNavComponent],
   templateUrl: './app-header.component.html',
 })
 export class AppHeader {
