38069d8a99102d0ee42e89ad7b5783fa1b187ab9
Isai Boussif <eexdhe@gmail.com>
2023-11-21 08:59:00 +0100

    feat: add helmet

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {ConfigService} from "@nestjs/config";
+import { ConfigService } from '@nestjs/config';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService)
-  const port = configService.get<number>('PORT') ?? 3000
+  app.use(helmet());
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT') ?? 3000;
   await app.listen(port);
 }
+
 bootstrap();
