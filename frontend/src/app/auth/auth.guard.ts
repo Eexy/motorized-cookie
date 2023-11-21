c5cb89e936ff8dc9ce0233597c25717331d2f24b
Isai Boussif <eexdhe@gmail.com>
2023-11-21 19:05:22 +0100

    feat: use env variable

--- frontend/src/app/auth/auth.guard.ts
+++ frontend/src/app/auth/auth.guard.ts
@@ -1,11 +1,14 @@
 import {CanActivateFn, Router} from "@angular/router";
 import {AuthService} from "./services/auth.service";
 import {inject} from "@angular/core";
+import {environment} from "../../environments/environment";
 
 export function authGuard(): CanActivateFn {
   return () => {
     const authService: AuthService = inject(AuthService)
     const router: Router = inject(Router)
+
+    if (!environment.production) return true
 
     if (!authService.getToken()) {
       router.navigate(["/auth/signup"])
--- frontend/src/app/auth/services/auth.service.ts
+++ frontend/src/app/auth/services/auth.service.ts
@@ -3,6 +3,7 @@ import {HttpClient} from "@angular/common/http";
 import {SignupDto} from "../dto/signup.dto";
 import {SigninDto} from "../dto/signin.dto";
 import {Router} from "@angular/router";
+import {environment} from "../../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
@@ -12,13 +13,22 @@ export class AuthService {
   }
 
   async signup(signupDto: SignupDto) {
-    this.http.post<{ token: string }>('http://127.0.0.1:3000/auth/signup', signupDto).subscribe(res => {
+    if (!environment.production) {
+      localStorage.setItem("token", "token")
+      return this.router.navigate([""])
+    }
+
+    return this.http.post<{ token: string }>('http://127.0.0.1:3000/auth/signup', signupDto).subscribe(res => {
       this.router.navigate([""])
     })
   }
 
   async signin(signinDto: SigninDto) {
-    this.http.post<{ token: string }>('http://127.0.0.1:3000/auth/signup', signinDto).subscribe(res => {
+    if (!environment.production) {
+      localStorage.setItem("token", "token")
+      return this.router.navigate([""])
+    }
+    return this.http.post<{ token: string }>('http://127.0.0.1:3000/auth/signup', signinDto).subscribe(res => {
       localStorage.setItem("token", res.token)
       this.router.navigate([""])
     })
