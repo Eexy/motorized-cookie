742228663504005d035a907fe7120bc208741aa6
Isai Boussif <eexdhe@gmail.com>
2023-11-21 20:03:37 +0100

    chore: create placeholder page for user

---
+++ frontend/src/app/user/pages/user-information/user-information.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+@Component({
+  selector: 'user-information',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './user-information.component.html',
+})
+export class UserInformationComponent {
+
+}
--- frontend/src/app/user/user.routes.ts
+++ frontend/src/app/user/user.routes.ts
@@ -1,12 +1,21 @@
 import {Routes} from "@angular/router";
 import {UserPageComponent} from "./pages/user-page/user-page.component";
 import {authGuard} from "../auth/auth.guard";
+import {UserInformationComponent} from "./pages/user-information/user-information.component";
+import {UserInvoiceComponent} from "./pages/user-invoice/user-invoice.component";
 
 export const userRoutes: Routes = [
   {
-    path: "users/me",
+    path: "users",
     component: UserPageComponent,
     canActivate: [authGuard()],
-    title: "user"
+    children: [{
+      path: "me",
+      component: UserInformationComponent
+    },
+      {
+        path: "invoices",
+        component: UserInvoiceComponent
+      }]
   }
 ]
