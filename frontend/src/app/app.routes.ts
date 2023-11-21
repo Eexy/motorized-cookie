5f0cc7bd092c5f83187ed3d158c223bec019dccc
Isai Boussif <eexdhe@gmail.com>
2023-11-21 20:12:51 +0100

    feat: make logo a link to homepage and fix route url

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,4 +2,7 @@ import {Routes} from '@angular/router';
 import {authRoutes} from "./auth/auth.routes";
 import {userRoutes} from "./user/user.routes";
 
-export const routes: Routes = [...authRoutes, ...userRoutes];
+export const routes: Routes = [
+  ...authRoutes,
+  ...userRoutes
+];
--- frontend/src/app/components/app-header/app-header.component.ts
+++ frontend/src/app/components/app-header/app-header.component.ts
@@ -1,11 +1,12 @@
 import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HeaderCtasComponent} from "./components/header-ctas/header-ctas.component";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, HeaderCtasComponent],
+  imports: [CommonModule, HeaderCtasComponent, RouterLink],
   templateUrl: './app-header.component.html',
 })
 export class AppHeader {
