b4ef0a4e0244839ff89545b6df083ca2bd4dd3f6
Isai Boussif <eexdhe@gmail.com>
2023-11-21 09:11:21 +0100

    feat: init angular app

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,9 @@ import helmet from 'helmet';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
+  app.enableCors({
+    origin: '*',
+  });
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT') ?? 3000;
   await app.listen(port);
