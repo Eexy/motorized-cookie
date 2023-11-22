bf62eebad8677562648aa05cca8c4e624ca27d82
Isai Boussif <eexdhe@gmail.com>
2023-11-22 16:54:11 +0100

    feat: add route to get product by id

--- backend/src/products/products.controller.ts
+++ backend/src/products/products.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { ProductsService } from './products.service';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+  constructor(private readonly productService: ProductsService) {}
+
+  @Get(':id')
+  async findById(@Param('id', ParseIntPipe) id: number) {
+    const product = await this.productService.findById(id);
+
+    if (!product) {
+      throw new NotFoundException(`Unable to found product with id ${id}`);
+    }
+
+    return product;
+  }
+}
--- backend/src/products/products.service.ts
+++ backend/src/products/products.service.ts
@@ -1,4 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { DB, DbType } from '../database/database.provider';
+import { eq } from 'drizzle-orm';
+import { products } from '../database/schema';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  constructor(@Inject(DB) private readonly db: DbType) {}
+
+  async findById(id: number) {
+    return await this.db.query.products.findFirst({
+      where: eq(products.id, id),
+      with: {
+        categories: true,
+      },
+    });
+  }
+}
