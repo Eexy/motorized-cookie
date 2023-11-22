6ac1a6c43447d2e6fccc5e43bcb386b4caefa50d
Isai Boussif <eexdhe@gmail.com>
2023-11-22 03:31:05 +0100

    feat: send signup email

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -8,20 +8,32 @@ import { ClientsService } from '../clients/clients.service';
 import { SignupDto } from './dto/signupDto';
 import { SigninDto } from './dto/signin.dto';
 import { JwtService } from '../jwt/jwt.service';
+import { MailService } from '../mail/mail.service';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly clientsService: ClientsService,
     private readonly jwtService: JwtService,
+    private readonly mailService: MailService,
   ) {}
 
+  /**
+   * Register new client in DB, send signup email and return auth token
+   */
   async signup(signupDto: SignupDto) {
     const hashedPassword = await argon2.hash(signupDto.password);
     const client = await this.clientsService.createUser({
       ...signupDto,
       password: hashedPassword,
     });
+
+    await this.mailService.sendEmail(
+      client[0].email!,
+      'Signup',
+      '<p>Welcome to this website new client</p>',
+    );
+
     return this.jwtService.generateJwt({
       sub: client[0].id,
       email: client[0].email,
