5dea33b4a4e00110cb965f19459059e3f9d73d55
Isai Boussif <eexdhe@gmail.com>
2023-11-23 16:16:40 +0100

    feat: define default value for env

--- backend/src/env/env.schema.ts
+++ backend/src/env/env.schema.ts
@@ -2,8 +2,11 @@ import { z } from 'zod';
 
 export const envSchema = z.object({
   PORT: z.coerce.number().default(3000),
-  DB_URL: z.string().trim(),
-  JWT_SECRET: z.string().trim(),
+  DB_URL: z
+    .string()
+    .trim()
+    .default('postgresql://postgres:postgres@postgres:5432/postgres'),
+  JWT_SECRET: z.string().trim().default('jwtsecretfortoken'),
   MAIL_HOST: z.string().optional(),
   MAIL_SERVICE: z.string().optional(),
   MAIL_SECURE: z.coerce.boolean().default(false),
