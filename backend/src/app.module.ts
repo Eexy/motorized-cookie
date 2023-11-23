2b358e63783e4789b09164fe9330d11293b0077b
Isai Boussif <eexdhe@gmail.com>
2023-11-23 16:35:34 +0100

    chore: remove env from backend dockerfile to use default value

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { validate } from './env/env.validation';
 import { DatabaseModule } from './database/database.module';
 import { ClientsModule } from './clients/clients.module';
 import { AuthModule } from './auth/auth.module';
@@ -10,12 +9,13 @@ import { JwtModule } from './jwt/jwt.module';
 import { CategoriesModule } from './categories/categories.module';
 import { MailModule } from './mail/mail.module';
 import { ProductsModule } from './products/products.module';
+import { validate } from './env/env.validation';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      validate,
+      validate: validate,
     }),
     DatabaseModule,
     JwtModule,
